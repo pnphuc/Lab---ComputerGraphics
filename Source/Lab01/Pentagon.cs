59e20a12ca2d32bae3b0550d7e454690ded1b7c6
pnphuc.creative@gmail.com <pnphuc.creative@gmail.com>
2023-01-04 19:52:07 +0700

    First

---
+++ Source/Lab01/Pentagon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Pentagon:Polygon
+    {
+        public Pentagon()
+        {
+            nPoly = 5;
+            init();
+        }
+
+        public override void set(Point A, Point B)
+        {
+            base.set(A, B);
+            p2r.Y = p2.Y = p1.Y + (p2.X - p1.X);
+            int rx = Math.Abs(p1.X - p2.X) / 2, ry = Math.Abs(p1.Y - p2.Y) / 2;
+
+            int x18 = (int)Math.Round(rx * Math.Cos(18 * Math.PI / 180)),
+                x54 = (int)Math.Round(rx * Math.Cos(54 * Math.PI / 180)),
+                y18 = (int)Math.Round(ry * Math.Sin(18 * Math.PI / 180)),
+                y54 = (int)Math.Round(ry * Math.Sin(54 * Math.PI / 180));
+            nPoints[0].setPoint(0, ry);
+            nPoints[1].setPoint(x18, y18);
+            nPoints[2].setPoint(x54, -y54);
+            nPoints[3].setPoint(-x54, -y54);
+            nPoints[4].setPoint(-x18, y18);
+        }
+    }
+}
